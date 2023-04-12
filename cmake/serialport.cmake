f8c8fb725c561a8b55e9adfbc248c6496da2ef6a
Martin Gabel <marting@becker-antriebe.com>
2023-04-12 13:13:22 +0200

    static link fix?

--- cmake/serialport.cmake
+++ cmake/serialport.cmake
@@ -1,6 +1,7 @@
 
 include(ExternalProject)
 
+set(CMAKE_POSITION_INDEPENDENT_CODE ON)
 set(SERIALPORT_BIN ${CMAKE_BINARY_DIR}/libserialport)
 set(SERIALPORT_STATIC_LIB ${CMAKE_BINARY_DIR}/libserialport/lib/libserialport.a)
 ExternalProject_Add(libserialport
@@ -8,8 +9,8 @@ ExternalProject_Add(libserialport
   BUILD_IN_SOURCE ON
   GIT_REPOSITORY https://github.com/sigrokproject/libserialport.git
   GIT_TAG libserialport-0.1.1
-  UPDATE_DISCONNECTED ON
-  CONFIGURE_COMMAND "./autogen.sh" COMMAND "./configure" ARGS "--prefix=${SERIALPORT_BIN}"
+  UPDATE_DISCONNECTED OFF
+  CONFIGURE_COMMAND "./autogen.sh" COMMAND "./configure" ARGS "-C" "CCFLAGS=-g -O2 -fPIC" "CXXFLAGS=-g -O2 -fPIC" "--with-pic=yes" "--prefix=${SERIALPORT_BIN}"
   BUILD_COMMAND "make"
   INSTALL_COMMAND make DEST=${SERIALPORT_BIN} install
   BUILD_BYPRODUCTS ${SERIALPORT_STATIC_LIB}
@@ -22,4 +23,5 @@ message(INFO " libserialport SOURCE_DIR: '${INSTALL_DIR}'")
 add_library(serialport STATIC IMPORTED GLOBAL)
 add_dependencies(serialport libserialport)
 set_target_properties(serialport PROPERTIES IMPORTED_LOCATION ${SERIALPORT_STATIC_LIB})
+set_target_properties(serialport PROPERTIES POSITION_INDEPENDENT_CODE ON)
 set_target_properties(serialport PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${SOURCE_DIR})
--- include/register_types.h
+++ include/register_types.h
@@ -3,9 +3,11 @@
 
 #include <godot_cpp/core/class_db.hpp>
 
+namespace godot {
 void
 init_gdserialport(godot::ModuleInitializationLevel p_level);
 void
 uninit_gdserialport(godot::ModuleInitializationLevel p_level);
+};
 
 #endif // EXAMPLE_REGISTER_TYPES_H
